23503bfac4a636a7d55552533767a5fdc2d6e763
tushar-bht <tusharbhtt67@gmail.com>
2020-06-19 01:42:02 +0530

    1st commit

---
+++ index.js
@@ -0,0 +1,139 @@
+console.log("document is loaded");
+//infix to postfix converter .......
+var operatorStack = []; //for storing operators.......
+var outputArr = []; //for storing output........
+function isOperator(char) {
+  switch (char) {
+    case "++":
+    case "--":
+    case "~":
+    case "!":
+    case "&": //unary operator HIGHEST PRECEDENCE.......
+    case "^":
+    case "$": //2nd HIGHEST PRECIDENCE.....
+    case "*":
+    case "/":
+    case "%": //3rd HIGHEST PRECEDENCE .......
+    case "+":
+    case "-": //4th HIGHTEST PRECEDENCE .........
+    case "<":
+    case ">":
+    case "<=":
+    case ">=": //5th HIGHEST PRECEDENCE ..........
+    case "==":
+    case "!=": //6th HIGHEST PRECEDENCE .............
+    case "&&": //7th HIGHEST PRECEDENCE .............
+    case "||": //8th HIGHEST PRECEDENCE .............
+    case "?:": //9th HIGHEST PRECEDENCE .............
+    case "=":
+    case "+=":
+    case "-=":
+    case "*=": //10th HIGHTEST PRECEDENCE ............
+
+    case "(":
+    case "#":
+      return 1;
+    default:
+      return 0;
+  }
+}
+function operatorNo(char) {
+  //assigning a number to the operators according to precedence........
+  if (
+    char === "++" ||
+    char === "--" ||
+    char === "~" ||
+    char === "!" ||
+    char === "&"
+  )
+    return 1;
+  else if (char === "^" || char === "$") return 2;
+  else if (char === "*" || char === "/" || char === "%") return 3;
+  else if (char === "+" || char === "-") return 4;
+  else if (char === ">" || char === "<" || char === "<=" || char === ">=")
+    return 5;
+  else if (char === "==" || char === "!=") return 6;
+  else if (char === "&&") return 7;
+  else if (char === "||") return 8;
+  else if (char === "?:") return 9;
+  else return 10;
+}
+function placeOperator(char) {
+  if (char === "(") {
+    operatorStack.push(char);
+    return 0;
+  }
+  var currentOperatorReplacingPower = operatorNo(char);
+  console.log(
+    "currentOperatorReplacingPower is " + currentOperatorReplacingPower
+  );
+  var stackOperatorReplacingPower = operatorNo(
+    operatorStack[operatorStack.length - 1]
+  );
+  if (currentOperatorReplacingPower < stackOperatorReplacingPower) {
+    //if current operator percedence is highter than that in the stack top
+    operatorStack.push(char);
+  } else {
+    while (
+      currentOperatorReplacingPower >= stackOperatorReplacingPower &&
+      operatorStack.length > 0
+    ) {
+      outputArr.push(operatorStack[operatorStack.length - 1]);
+      operatorStack.pop();
+      stackOperatorReplacingPower = operatorNo(
+        operatorStack[operatorStack.length - 1]
+      );
+    }
+    operatorStack.push(char);
+  }
+}
+function handleClosingBracket(char) {
+  for (let i = operatorStack.length - 1; i >= 0; i--) {
+    if (operatorStack[i] === "(") {
+      operatorStack.pop();
+      break;
+    } else {
+      outputArr.push(operatorStack[i]);
+      operatorStack.pop();
+    }
+  }
+}
+function postfix(arr) {
+  //main function ..........
+  for (let i = 0; i < arr.length; i++) {
+    var table = document.getElementById("table");
+    var tableRow = document.createElement("tr");
+    var tableData = document.createElement("Td");
+
+    let char = arr[i]; //picking every element of input individually......
+    tableData.innerHTML = char;
+    tableRow.appendChild(tableData);
+    if (isOperator(char) === 1) {
+      placeOperator(char); //placing the operator in the write position.........
+    } else if (char == ")") {
+      handleClosingBracket(char);
+    } else outputArr.push(char);
+    tableData.innerHTML = operatorStack.join(" ");
+    tableRow.appendChild(tableData);
+    tableData.innerHTML = outputArr.join(" ");
+    tableRow.appendChild(tableData);
+    table.appendChild(tableRow);
+  }
+}
+function handleClick() {
+  var input = document.getElementById("expression").value;
+  var input = "( " + input + " )";
+  var inputArr = input.split(" "); //splitting the input by spaces .......
+
+  postfix(inputArr);
+  console.log("outputArr " + outputArr);
+  console.log("operatorStack " + operatorStack);
+  var answer = outputArr.join(" ");
+  var output = document.getElementById("displayOutput");
+  output.innerHTML = answer;
+  document.getElementById("answer").classList.remove("hidden");
+
+  outputArr = [];
+  operatorStack = [];
+}
+console.log(document.getElementById("het").innerHTML);
